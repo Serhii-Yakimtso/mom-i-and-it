7717aae0a758530043ca264f05d488eb6eba9830
EmaxxD <97693129+EmaxxD@users.noreply.github.com>
2023-11-28 21:42:46 +0200

    add js, add sticky header, add header-position

---
+++ src/navigation.js
@@ -0,0 +1,17 @@
+// /============= Mobile menu============/;
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-open]'),
+    closeModalBtn: document.querySelector('[data-modal-close]'),
+    modal: document.querySelector('[data-modal]'),
+    menuLink: document.querySelectorAll('.menu-modal-nav-link'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.menuLink.forEach(link => link.addEventListener('click', toggleModal));
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-open');
+  }
+})();
